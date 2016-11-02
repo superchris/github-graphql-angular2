40039271e9165b8d1f082b15c07a943d00b36a7e
Chris Nelson <chris.nelson@kroger.com>
2016-11-02 19:15:44 -0400

    basic query works i think

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { Angular2Apollo } from 'angular2-apollo';
+import gql from 'graphql-tag';
+import "rxjs/Rx";
 
 @Component({
   selector: 'app-root',
@@ -7,4 +10,68 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app works!';
+  searchArgs: any = {};
+  queryResults: any;
+  lastCursor: String;
+
+  constructor(private apolloClient: Angular2Apollo) {
+  }
+
+  prevPage() {
+    console.log("previousing");
+  }
+
+  nextPage() {
+    console.log("nexting");
+
+    this.executeQuery({
+      query: this.searchArgs.query,
+      after: this.lastCursor,
+      before: '',
+      type: 'REPOSITORY'
+    });
+  }
+
+  search(event) {
+    this.executeQuery({
+      query: this.searchArgs.query,
+      after: '',
+      before: '',
+      type: 'REPOSITORY'
+    });
+  }
+
+  executeQuery(variables) {
+    this.queryResults = this.apolloClient.watchQuery({
+      query: gql`
+      query($query: String!, $type: SearchType!, $after: String, $before: String) {
+        search(first: 10, type: $type, query: $query, after: $after, before: $before) {
+          pageInfo {
+            startCursor,
+            endCursor,
+            hasNextPage,
+            hasPreviousPage
+          }
+          edges {
+            cursor
+            node {
+              ... on Repository {
+                name,
+                owner {
+                  id,
+                  login
+                }
+              }
+            }
+          }
+        }
+      }
+      `,
+      variables
+    }).map((data) => {
+      const edges = data.data.search.edges;
+      this.lastCursor = edges[edges.length - 1].cursor;
+      return edges.map(edge => edge.node);
+    });
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,17 +2,40 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
-
+import ApolloClient, { createNetworkInterface } from 'apollo-client';
+import { ApolloModule } from 'angular2-apollo';
 import { AppComponent } from './app.component';
+import { PaginationModule } from 'ng2-bootstrap';
+
+const token = '7ee278836442d6c3c6fa9ad287b034963ce1463a';
+let networkInterface = createNetworkInterface('https://api.github.com/graphql');
+const client = new ApolloClient({
+  // networkInterface: createNetworkInterface('http://localhost:53441/')
+  networkInterface
+  // networkInterface: createNetworkInterface('http://localhost:4000/graphql')
+  // networkInterface: createNetworkInterface('https://www.graphqlhub.com/graphql')
+});
+
+networkInterface.use([{
+  applyMiddleware(req, next) {
+    if (!req.options.headers) {
+      req.options.headers = {};  // Create the header object if needed.
+    }
+    req.options.headers['authorization'] = `bearer ${token}`;
+    next();
+  }
+}]);
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
+    PaginationModule,
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    ApolloModule.withClient(client)
   ],
   providers: [],
   bootstrap: [AppComponent]
